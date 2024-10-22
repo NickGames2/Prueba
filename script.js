201dd8deb9d630332ff582b1926b1cfae7cd6d5f
NickGames2 <jlagudelo5@gmail.com>
2024-10-22 18:33:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+function changeMessage() {
+    const message = document.getElementById("message");
+    message.innerText = "Siempre serás mi mejor amiga, mi confidente y mi mayor apoyo. No importa dónde estemos o lo que pase, siempre estarás en mi corazón. Gracias por ser la hermana maravillosa que eres.";
+}
